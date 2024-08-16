71bddbfc78a360892f5df73f3711df53e67013bf
MaratXcode <kosjachok8@yandex.ru>
2024-08-16 20:18:18 +0300

    Initial Commit

---
+++ Fuel Meter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fuel Meter
+//
+//  Created by Marat Fakhrizhanov on 16.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fuel Meter/Fuel_MeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fuel_MeterApp.swift
+//  Fuel Meter
+//
+//  Created by Marat Fakhrizhanov on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Fuel_MeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
