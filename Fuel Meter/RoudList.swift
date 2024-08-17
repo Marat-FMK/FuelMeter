9abee7abe169e0f232170c7e1c23e7748a76db1f
MaratXcode <kosjachok8@yandex.ru>
2024-08-17 16:26:05 +0300

    work with rouds view

---
+++ Fuel Meter/RoudList.swift
@@ -0,0 +1,18 @@
+//
+//  RoudList.swift
+//  Fuel Meter
+//
+//  Created by Marat Fakhrizhanov on 17.08.2024.
+//
+
+import SwiftUI
+
+struct RoudList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoudList()
+}
