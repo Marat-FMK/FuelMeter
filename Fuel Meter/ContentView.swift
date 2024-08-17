598d4d85a365ab7581cab3d695746bc63df50498
MaratXcode <kosjachok8@yandex.ru>
2024-08-17 16:25:38 +0300

    work with button and logic

--- Fuel Meter/ContentView.swift
+++ Fuel Meter/ContentView.swift
@@ -9,116 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var fuelСonsumption = ""
-    @State var distance = ""
-    @State var price = ""
-    @State private var isPresented: Bool = false
-   
-    @State var summ: Double = 0.0
-    
     var body: some View {
-        ZStack {
-            Image("pic2")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(.all)
-                .opacity(0.6)
-            VStack (alignment: .leading) {
-                
-                Spacer()
-                
-                Text("Расход топлива")
-                    .font(.largeTitle)
-                TextField( "  л/100км", text: $fuelСonsumption)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 17)
-                            .stroke(lineWidth: 2)
-                            .foregroundStyle(Color.purple)
-                    )
-                    .frame(width: 300,height: 30)
-                    .font(.largeTitle)
-                    .padding()
-                
-                
-                Text("Дистанция")
-                    .font(.largeTitle)
-                TextField( "  км ", text: $distance)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 17)
-                            .stroke(lineWidth: 2)
-                            .foregroundStyle(Color.purple)
-                    )
-                    .frame(width: 300,height: 30)
-                    .font(.largeTitle)
-                    .padding()
-                
-                
-                Text("Цена за литр")
-                    .font(.largeTitle)
-                TextField( "  руб ", text: $price)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 17)
-                            .stroke(lineWidth: 2)
-                            .foregroundStyle(Color.purple)
-                    )
-                    .frame(width: 300,height: 30)
-                    .font(.largeTitle)
-                    .padding()
-                
-                Spacer()
-                
-                Button ( action: {
-                    isPresented = true
-                    
-                   let fue = Double(fuelСonsumption) ?? 0
-                    let dis = Double(distance) ?? 0
-                    let pri = Double(price) ?? 0
-                    
-                    summ = (( dis * fue ) / 100 ) * pri
-                    
-                            
-                            
-//                    let liters = ( Double(distance) ?? 0 * (Double(fuelСonsumption) ?? 0) ) / 100
-//                    
-//                    summ = liters * (Double(price) ?? 0)
-                    
-                    
-                } ) {
-                    Text("Рассчитать")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundStyle(Color.black)
+     
+        TabView {
+            TextFieldsView()
+                .tabItem {
+                    Image(systemName: "gauge.open.with.lines.needle.67percent.and.arrowtriangle.and.car")
+                    Text("Главная")
                 }
-                .sheet(isPresented: $isPresented, content: {
-                    ResultView(summ: $summ)
-                })
-                .padding()
-                .frame(width: 250, height: 30, alignment: .center)
-                .background(Color.red)
-                .cornerRadius(20)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(Color.orange, lineWidth: 3))
-                .offset(x: 40)
-                
-                Spacer()
-            }
-            .padding()
+            RoudList()
+                .tabItem {
+                    Image(systemName: "map.fill")
+                    Text("Rouds")
+                    
+                }
+            
         }
+        
+        
+        
     }
     
-    private func calculation() -> Double {
-        isPresented = true
-        let liters = ( Double(distance) ?? 0 * (Double(fuelСonsumption) ?? 0) ) / 100
-        let summ = liters * (Double(price) ?? 0)
-        return summ
-    }
-    private func checkCharacter(test: String) {
-        if let _ = Double(test) {
-            price = ""
-            return
-        }
-    }
 }
 
 #Preview {
--- Fuel Meter/ResultView.swift
+++ Fuel Meter/ResultView.swift
@@ -11,13 +11,47 @@ struct ResultView: View {
     @Binding var summ: Double
     var body: some View {
         
-        Text("Сумма за поездку")
-            .font(.largeTitle)
-        Text("\(summ)")
-            .padding()
-        
+        ZStack {
+            Image("pic3")
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(Color.gray)
+                        .opacity(0.9)
+                        .frame(width: 310, height: 200)
+                        .cornerRadius(30)
+                    
+                    
+                    
+                    
+                    VStack { Text("Сумма за поездку")
+                            .font(.largeTitle)
+                        
+                        Text(String(format: "%.1f",summ ))
+                            .padding()
+                    }
+                }.offset(y: -90)
+              
+                Button (action: {} ) {
+                    Text("OK")
+                        .frame(width: 100, height: 50)
+                        .foregroundStyle(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(20)
+                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.red, lineWidth: 3))
+                        .shadow(radius: 20)
+                }
+            }
+        }
+    }
+    
+    
+    private func dismissView() {
+    
     }
 }
+    
+
 
 #Preview {
     ResultView(summ: .constant(1200))
