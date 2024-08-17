bb3a9d5492bccc003785903cab09c03b3294f661
MaratXcode <kosjachok8@yandex.ru>
2024-08-17 17:58:00 +0300

    work with about road view

---
+++ Fuel Meter/AboutRoudView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutRoudView.swift
+//  Fuel Meter
+//
+//  Created by Marat Fakhrizhanov on 17.08.2024.
+//
+
+import SwiftUI
+
+struct AboutRoudView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutRoudView()
+}
--- Fuel Meter/FuelMeterApp.swift
+++ Fuel Meter/FuelMeterApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct Fuel_MeterApp: App {
+struct FuelMeterApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
